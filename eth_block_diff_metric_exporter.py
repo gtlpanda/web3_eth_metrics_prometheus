245971fe7c3d32683cd97bb2608de35681ce4e94
Carlos <c.feliciano79@gmail.com>
2021-10-06 07:26:02 -0700

    removing personal stuff

--- eth_block_diff_metric_exporter.py
+++ eth_block_diff_metric_exporter.py
@@ -5,11 +5,12 @@ import time
 
 from web3 import Web3
 
-w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/8c300834a4484968b3b8556dbbb38ece"))
+w3 = Web3(Web3.HTTPProvider("<INSERT INFURA URL HERE"))
 
 # RANDOM_NUMBER_GAUGE = prom.Gauge('rendom_number_gauge', 'Random number between 1 - 100')
 
-RANDOM_NUMBER_GAUGE = prom.Gauge('eth_difficulty', 'Here is the eth difficulty:')
+RANDOM_NUMBER_GAUGE = prom.Gauge(
+    'eth_difficulty', 'Here is the eth difficulty:')
 
 
 # def generate_random_numbers():
@@ -23,7 +24,6 @@ def get_latest_block_diff_eth():
         latest_diff_eth = w3.eth.get_block("latest").difficulty
         RANDOM_NUMBER_GAUGE.set(latest_diff_eth)
         time.sleep(5)
-
 
 
 if __name__ == '__main__':
